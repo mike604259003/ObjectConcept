7793ddc5308ce09696a129388c1623d791ef707a
mike604259003 <52741163+mike604259003@users.noreply.github.com>
2019-07-22 16:18:50 +0700

    Update Class 2

--- src/th/ac/npru/swe/porntape/test/TestDriver.java
+++ src/th/ac/npru/swe/porntape/test/TestDriver.java
@@ -1,18 +1,23 @@
 package th.ac.npru.swe.porntape.test;
 
 import th.ac.npru.swe.porntape.oop.Rectangle;
+import th.ac.npru.swe.porntape.oop.Triangle;
 
 public class TestDriver {
 
 	public static void main(String[] args) {
-		double area;
+		
 
 		Rectangle r1 = new Rectangle(4, 6);
-		area = r1.getArea();
+		r1.getArea();
 		r1.setColor("Red");
-		System.out.print(area);
-		System.out.print(r1.toString());
-
+		System.out.println(r1.toString());
+		
+		
+		Triangle t1 = new Triangle(5,10);
+		t1.getArea();
+		t1.setColor("Pink");
+		System.out.print(t1.toString());
 	}
  
 }
